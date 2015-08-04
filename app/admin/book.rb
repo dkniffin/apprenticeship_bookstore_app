4eb8c0ecd0f860446082bd49eaf5273679ac9847
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-04 14:12:10 -0400

    Added category to book

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Book do
-  permit_params :title, :published_date, :author
+  permit_params :title, :published_date, :author, :category
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
