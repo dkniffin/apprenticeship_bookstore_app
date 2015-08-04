e913dc5e3a5e26410c21dbb991e5e24d197062a4
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-04 16:28:19 -0400

    Added sort order to book index, plus some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 AdminUser.create :email => 'someuser@example.com', :password => 'password123!'
+
+def rand_date
+  rand(15340).days.ago(Date.today)
+end
+Book.create([
+{ :title => "I Win!", :author => "U. Lose", :published_date => rand_date},
+{ :title => "Robots", :author => "Anne Droid", :published_date => rand_date},
+{ :title => "Danger!", :author => "Luke Out", :published_date => rand_date},
+{ :title => "Cloning", :author => "Irma Dubble II", :published_date => rand_date},
+{ :title => "Hot Dog!", :author => "Frank Furter", :published_date => rand_date},
+{ :title => "Cry Wolf", :author => "Al Armist", :published_date => rand_date},
+{ :title => "Wake Up!", :author => "Sal Ammoniac", :published_date => rand_date},
+{ :title => "I'm Fine", :author => "Howard Yu", :published_date => rand_date},
+{ :title => "Gambling", :author => "Monty Carlos", :published_date => rand_date},
+{ :title => "Mensa Man", :author => "Gene Yuss", :published_date => rand_date},
+{ :title => "Big Fart!", :author => "Hugh Jass", :published_date => rand_date},
+{ :title => "Hypnotism", :author => "N. Tranced", :published_date => rand_date},
+{ :title => "Downpour!", :author => "Wayne Dwops", :published_date => rand_date},
+{ :title => "Full Moon", :author => "Seymour Buns", :published_date => rand_date},
+{ :title => "Sea Birds", :author => "Al Batross", :published_date => rand_date},
+{ :title => "Teach Me!", :author => "I. Wanda Know", :published_date => rand_date},
+])
