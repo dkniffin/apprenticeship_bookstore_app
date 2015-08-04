c9569180a2c8481fb903ba47e0aef4cc73e8cc44
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-04 13:07:17 -0400

    Started setting up admin via devise, but ran into issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803193924) do
+ActiveRecord::Schema.define(version: 20150803194742) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150803193924) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
