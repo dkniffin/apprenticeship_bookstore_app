bccff719224bf4c4e7fb9bbb8dab939d31907b91
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-04 14:32:45 -0400

    Added spec for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { ['test@nowhere.com','abc@xyz.net','joe.schmoe@somewhere.edu'].sample }
+    password { ['password','password123','qwerty123!','$Up3|^$3(uR3'].sample }
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.new }
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password) }
+
+  describe "valid user" do
+    subject(:user) { build(:user) }
+
+    it "is valid" do
+      expect(user).to be_valid
+    end
+    it "has an email" do
+      expect(user.email).to be_a(String)
+    end
+    it "has a password" do
+      expect(user.password).to be_a(String)
+    end
+  end
+  describe "invalid user" do
+    let(:no_email) { build(:user, email: nil) }
+    let(:no_password) { build(:user, password: nil) }
+
+    it "is invalid without email" do
+      expect(no_email).to_not be_valid
+      expect(no_email.errors).to include(:email)
+      expect(no_email.errors[:email]).to include("can't be blank")
+    end
+    it "is invalid without password" do
+      expect(no_password).to_not be_valid
+      expect(no_password.errors).to include(:password)
+      expect(no_password.errors[:password]).to include("can't be blank")
+    end
+  end
 end
