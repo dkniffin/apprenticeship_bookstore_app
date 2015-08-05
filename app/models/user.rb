7299ae4f6317542c72864d4c1b74ae5ab31d3082
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-05 16:45:29 -0400

    Added ability for user to add books to cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :orders
+
+  def cart
+    if last_order.nil? || last_order.completed?
+      orders << Order.new({completed: false})
+    end
+    last_order
+  end
+
+  private
+    def last_order
+      orders.last
+    end
 end
