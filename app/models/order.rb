ab950524be4fec7cefe4d6b056e711f44f8ecdc7
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-05 11:34:03 -0400

    Added association between order and user

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :line_items
+  belongs_to :user
 
   validates :completed, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805134220) do
+ActiveRecord::Schema.define(version: 20150805153053) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -67,10 +67,10 @@ ActiveRecord::Schema.define(version: 20150805134220) do
   add_index "line_items", ["order_id"], name: "index_line_items_on_order_id"
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "user_id"
     t.boolean  "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
