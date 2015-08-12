ad88b15dc2faa67e44a57536d70098caece37f74
oddityoverseer13 <derek.kniffin@gmail.com>
2015-08-12 09:46:30 -0400

    Made a few changes to devise & mailer

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,7 @@
-ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => ENV['GMAIL_DOMAIN'],
-    :user_name            => ENV['GMAIL_USER'],
-    :password             => ENV['GMAIL_PW'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+# ActionMailer::Base.smtp_settings = {
+#     :address              => "smtp.gmail.com",
+#     :port                 => 587,
+#     :user_name            => ENV['gmail_username'],
+#     :password             => ENV['gmail_password'],
+#     :authentication       => 'plain',
+#     :enable_starttls_auto => true  }
